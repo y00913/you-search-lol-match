1ca6394eea62e8bd7f235e7f02a49ccbd8eb4f07
ayt_home <y00913@naver.com>
2022-09-02 22:06:44 +0900

    fix: 에러 페이지 변경

--- src/main/java/com/example/lol/controller/ExceptionHandlingController.java
+++ src/main/java/com/example/lol/controller/ExceptionHandlingController.java
@@ -3,14 +3,15 @@ package com.example.lol.controller;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ExceptionHandlingController implements ErrorController {
-    @RequestMapping("/error")
+
+    @GetMapping("/error")
     public String handleError(HttpServletRequest request) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
 
@@ -30,4 +31,5 @@ public class ExceptionHandlingController implements ErrorController {
 
         return "/error/error";
     }
+
 }
